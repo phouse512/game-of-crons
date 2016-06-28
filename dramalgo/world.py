0c78a364361c90c360655e45781cb1d8aa030881
phouse512 <philiphouse2015@u.northwestern.edu>
2016-06-28 18:45:31 -0500

    woop

--- dramalgo/world.py
+++ dramalgo/world.py
@@ -11,14 +11,22 @@ class World:
     basic blind constructor that creates an empty world
     """
     def __init__(self, width: int=20, height: int=20) -> None:
-        self.world = [[0 for x in range(width)] for y in range(height)]
-        print(self.world)
+        self.world = [["" for x in range(width)] for y in range(height)]
 
         for y in range(len(self.world)):
             for x in range(len(self.world[y])):
-                self.world[y][x] = "yes" # "y:%s x: %s" % (str(y), str(x))
+                self.world[y][x] = "y:%s x: %s" % (str(y), str(x))
 
-        print(self.world)
+        self.age = 0
+
+    def console_output(self, verbose: bool=False) -> None:
+
+        if verbose:
+            print("verbose")
+
+        print("\n\n")
+        print("Day: %s" % self.age)
+        print("=========")
 
 
 class Space:
